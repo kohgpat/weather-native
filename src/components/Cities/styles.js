880e1f253e72dc32ed379ddec808d506ec3ff8f1
Nikolay Burlov <kohgpat@gmail.com>
2018-02-25 05:11:37 +0800

    Fix paddings and margins in Cities

--- src/components/Cities/styles.js
+++ src/components/Cities/styles.js
@@ -1,8 +1,7 @@
 import styled from "styled-components/native";
 
 export const Cities = styled.FlatList`
-  margin-top: 80px;
-  padding: 20px;
+  padding: 0 20px 20px;
   flex: 1;
   flex-direction: column;
   width: 100%;
