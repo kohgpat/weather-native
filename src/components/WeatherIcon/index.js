05f0304b2ca378c6bf9cdc99964188064dde552b
Nikolay Burlov <kohgpat@gmail.com>
2018-02-25 04:51:21 +0800

    Fix WeatherIcon

--- src/components/WeatherIcon/index.js
+++ src/components/WeatherIcon/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as styles from "./styles";
 
+const ENDPOINT = "https://openweathermap.org/img/w/";
+
 const WeatherIcon = ({ weather }) => {
   if (!weather) {
     return null;
   }
 
-  return <styles.Icon icon={weather.icon} />;
+  return (
+    <styles.Icon
+      source={{
+        uri: `${ENDPOINT}${weather.icon}.png`
+      }}
+      icon={weather.icon}
+    />
+  );
 };
 
 WeatherIcon.propTypes = {
--- src/components/WeatherIcon/styles.js
+++ src/components/WeatherIcon/styles.js
@@ -1,14 +1,6 @@
 import styled from "styled-components/native";
 
-const ENDPOINT = "https://openweathermap.org/img/w/";
-
-export const Icon = styled.Image.attrs({
-  source: props => {
-    {
-      uri: `${ENDPOINT}${props.icon}.png`;
-    }
-  }
-})`
+export const Icon = styled.Image`
   width: 50px;
   height: 50px;
 `;
